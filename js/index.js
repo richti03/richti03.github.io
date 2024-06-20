018e0df647ba736df75def75d820d9128c975f95
DominikR02 <125642128+richti03@users.noreply.github.com>
2024-06-20 13:28:38 +0200

    Start Poject-Section

--- js/index.js
+++ js/index.js
@@ -11,13 +11,3 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
